9087f54cb2c9868dd54d5e08d23e32256774828d
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-04-02 12:10:36 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -5,14 +5,14 @@ from datetime import datetime
 from collections import defaultdict
 from plugins.Dreamxfutures.Imdbposter import get_movie_detailsx, fetch_image, get_movie_details
 from database.users_chats_db import db
-from kurigram import Client, filters, enums
+from pyrogram import Client, filters, enums
 from info import CHANNELS, MOVIE_UPDATE_CHANNEL, LINK_PREVIEW, ABOVE_PREVIEW, BAD_WORDS, LANDSCAPE_POSTER, TMDB_POSTER
 from Script import script
 from database.ia_filterdb import save_file
-from kurigram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils import temp
 from pymongo.errors import PyMongoError, DuplicateKeyError
-from kurigram.errors import MessageIdInvalid, MessageNotModified, FloodWait
+from pyrogram.errors import MessageIdInvalid, MessageNotModified, FloodWait
 from typing import Optional, Tuple
 
 logger = logging.getLogger(__name__)
